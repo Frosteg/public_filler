19f3ef9d1b4df65ed5e89c04bd9e37e41aafa7fd
Frost_ark <frost.ark.90@gmail.com>
2022-11-13 13:20:11 +0300

    with time tracking

--- exec/give_bot.py
+++ exec/give_bot.py
@@ -6,16 +6,16 @@ print('К отправке готов!')
 
 async def send_smthg():
     InputMediaPhoto = types.InputMediaPhoto
-    cur.execute("SELECT file_id FROM Content where sent = 0 LIMIT 1")                           #выбирает ссылку поста еще не отправленую
+    cur.execute("SELECT file_id FROM Next_to_send LIMIT 1")                           #выбирает ссылку поста еще не отправленую
     result = cur.fetchone()
     if result == None:
-        await bot.send_message(admin_id, "Контент закончился! Необходимо пополнить!")                                #Если реультат выборки None - отправить особщение
+        sender = await bot.send_message(admin_id, "Контент закончился! Необходимо пополнить!")                                #Если реультат выборки None - отправить особщение
     else:                                                                                   #Если есть результат - запустить процесс отправки
         last_sent = result[0]
-        cur.execute(f"SELECT group_id FROM Content where file_id = '{last_sent}'")                     #Выбрать ссылки по выбраному посту
+        cur.execute(f"SELECT group_id FROM Next_to_send where file_id = '{last_sent}'")                     #Выбрать ссылки по выбраному посту
         result_id = cur.fetchone()[0]
         if result_id != 'NotGroup':
-            cur.execute(f"SELECT * FROM Content where group_id = '{result_id}'")                     #Выбрать ссылки по выбраному посту
+            cur.execute(f"SELECT * FROM Next_to_send where group_id = '{result_id}'")                     #Выбрать ссылки по выбраному посту
             results_id = cur.fetchall()
             count = len(results_id)
             if count > 1:
@@ -36,32 +36,30 @@ async def send_smthg():
                             p = p+1
                             if p == maxtop:   
                                 break                 
-                    await bot.send_media_group(channel_id, media=mediagroup)
-                cur.execute(f"UPDATE Content set sent = '{up}' where group_id = '{result_id}'")        #обновление данных таблицы все вхождения по ссылке поста - отметить 1
+                    sender = await bot.send_media_group(channel_id, media=mediagroup)
+                cur.execute(f"UPDATE Content set sent = '{up}' where group_id = '{result_id}'")       #обновление данных таблицы все вхождения по ссылке поста - отметить 1
                 conn.commit()                    
-    if result_id == 'NotGroup':                                                                #если найдена 1 ссылка в таблице
-        cur.execute(f"SELECT mes_type FROM Content where file_id = '{last_sent}'")    
-        exp_id = cur.fetchone()[0]
-        print(f'Mestype: {exp_id}')
-        if exp_id == "Photo":                                                             #если категория Картинки
-            photo = last_sent
-            print(photo)
-            await bot.send_photo(channel_id, photo, send)
-            cur.execute(f"UPDATE Content set sent = '{up}' where file_id = '{last_sent}';")#обновление данных таблицы все вхождения по ссылке поста - отметить 1
-            conn.commit()
-        elif exp_id == "Video":                                                             #если категория Видео
-            photo = last_sent
-            await bot.send_video(channel_id,photo)
-            cur.execute(f"UPDATE Content set sent = '{up}' where file_id = '{last_sent}';")#обновление данных таблицы все вхождения по ссылке поста - отметить 1
-            conn.commit()
-        elif exp_id == "Animation":                                                             #если категория Видео
-            photo = last_sent
-            await bot.send_animation(channel_id, photo, caption=send)
-            cur.execute(f"UPDATE Content set sent = '{up}' where file_id = '{last_sent}';")#обновление данных таблицы все вхождения по ссылке поста - отметить 1
-            conn.commit()
-        elif exp_id == "Document":                                                             #если категория Видео
-            photo = last_sent
-            await bot.send_document(channel_id, photo, caption=send)
-            cur.execute(f"UPDATE Content set sent = '{up}' where file_id = '{last_sent}';")#обновление данных таблицы все вхождения по ссылке поста - отметить 1
-            conn.commit()
-        print('Отправлено')        
+        if result_id == 'NotGroup':                                                                #если найдена 1 ссылка в таблице
+            cur.execute(f"SELECT mes_type FROM Next_to_send where file_id = '{last_sent}'")    
+            exp_id = cur.fetchone()[0]
+            if exp_id == "Photo":                                                             #если категория Картинки
+                photo = last_sent
+                sender = await bot.send_photo(channel_id, photo, send)
+                cur.execute(f"UPDATE Content set sent = '{up}' where file_id = '{last_sent}';")#обновление данных таблицы все вхождения по ссылке поста - отметить 1
+                conn.commit()
+            elif exp_id == "Video":                                                             #если категория Видео
+                photo = last_sent
+                sender = await bot.send_video(channel_id,photo, caption=send)
+                cur.execute(f"UPDATE Content set sent = '{up}' where file_id = '{last_sent}';")#обновление данных таблицы все вхождения по ссылке поста - отметить 1
+                conn.commit()
+            elif exp_id == "Animation":                                                             #если категория Видео
+                photo = last_sent
+                sender = await bot.send_animation(channel_id, photo, caption=send)
+                cur.execute(f"UPDATE Content set sent = '{up}' where file_id = '{last_sent}';")#обновление данных таблицы все вхождения по ссылке поста - отметить 1
+                conn.commit()
+            elif exp_id == "Document":                                                             #если категория Видео
+                photo = last_sent
+                sender = await bot.send_document(channel_id, photo, caption=send)
+                cur.execute(f"UPDATE Content set sent = '{up}' where file_id = '{last_sent}';")#обновление данных таблицы все вхождения по ссылке поста - отметить 1
+                conn.commit()
+    return sender     
