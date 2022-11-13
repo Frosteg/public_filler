3e7a844df709a57d053e48af358af28aa9a20682
Frost_ark <frost.ark.90@gmail.com>
2022-11-13 13:18:47 +0300

    with time tracking

--- exec/presender.py
+++ exec/presender.py
@@ -1,22 +1,36 @@
 from aiogram import types
-from connector import  bot
-from data.config import send, cur, admin_id
+from connector import bot
+from data.config import channel_id, send, up, cur, conn, admin_id
 
 print('К отправке готов!')
 
+cur.execute("CREATE TABLE IF NOT EXISTS Next_to_send('file_id' BLOB, 'group_id' TEXT, 'mes_type' TEXT)")
+conn.commit() 
+
 async def presend_smthg():
     InputMediaPhoto = types.InputMediaPhoto
+    cur.execute("SELECT * FROM Next_to_send")
+    cur.execute("DELETE FROM Next_to_send")
+    conn.commit
     cur.execute("SELECT file_id FROM Content where sent = 0 LIMIT 1")                           #выбирает ссылку поста еще не отправленую
     result = cur.fetchone()
     if result == None:
-        await bot.send_message(admin_id, "Контент закончился! Необходимо пополнить!")                                #Если результат выборки None - отправить особщение
+        sender = await bot.send_message(admin_id, "Контент закончился! Необходимо пополнить!")                                #Если реультат выборки None - отправить особщение
     else:                                                                                   #Если есть результат - запустить процесс отправки
         last_sent = result[0]
-        cur.execute(f"SELECT group_id FROM Content where file_id = '{last_sent}'")                     #Проверить Group_id выбраннго поста
+        cur.execute(f"SELECT group_id FROM Content where file_id = '{last_sent}'")                     #Выбрать ссылки по выбраному посту
         result_id = cur.fetchone()[0]
         if result_id != 'NotGroup':
-            cur.execute(f"SELECT * FROM Content where group_id = '{result_id}'")                     #Выбрать ссылки по выбраному посту
+            cur.execute(f"SELECT * FROM Content where group_id = '{result_id}'")
             results_id = cur.fetchall()
+            for result in results_id:
+                row = result
+                file_id = row[0]
+                group_id = row[1]
+                mes_type = row[2]
+                inserting = (file_id, group_id, mes_type)
+                cur.execute(f"INSERT INTO Next_to_send (file_id,group_id, mes_type) VALUES (?,?,?);", inserting)
+                conn.commit()
             count = len(results_id)
             if count > 1:
                 group = len(results_id)                                                                   #подсчет количества ссылок
@@ -24,7 +38,7 @@ async def presend_smthg():
                 if group != 1:                                                                      #если количество ссылок больше 1 - формировать media_group
                     mediagroup = [] 
                     p = len(mediagroup) 
-                    for row in results_id:
+                    for row in results_id:                                                            #если категория - Картинки формировать в медиагруп и отправлять с помощью send_group_media
                         photo = row[0]
                         if p == 0:
                             mediagroup.append(InputMediaPhoto(photo, send, parse_mode='html'))
@@ -35,27 +49,27 @@ async def presend_smthg():
                             mediagroup.append(InputMediaPhoto(photo))
                             p = p+1
                             if p == maxtop:   
-                                break            
-                    await bot.send_media_group(admin_id, media=mediagroup)
-    if result_id == 'NotGroup':               
-        cur.execute(f"SELECT mes_type FROM Content where file_id = '{last_sent}'")    
-        exp_id = cur.fetchone()[0]
-        print(f'Mestype: {exp_id}')
-        if exp_id == "Photo":                                                             #если категория Картинки
-            photo = last_sent
-            await bot.send_photo(admin_id, photo, send)
-        elif exp_id == "Video":                                                             #если категория Видео
-            photo = last_sent
-            await bot.send_video(admin_id,photo)
-        elif exp_id == "Animation":                                                             #если категория Видео
-            photo = last_sent
-            await bot.send_animation(admin_id, photo, caption=send)
-        elif exp_id == "Document":                                                             #если категория Видео
-            photo = last_sent
-            await bot.send_document(admin_id, photo, caption=send)
-
-'''
-@dp.message_handler(commands=['sender'])
-async def process_parser_command(message: types.Message):
-    await send_smthg()
-'''
+                                break                 
+                    sender = await bot.send_media_group(admin_id, media=mediagroup)
+        if result_id == 'NotGroup':
+            cur.execute(f"SELECT * FROM Content where file_id = '{last_sent}'") 
+            row = cur.fetchall()[0]
+            file_id = row[0]
+            group_id = row[1]
+            mes_type = row[2]
+            inserting = (file_id, group_id, mes_type)
+            cur.execute(f"INSERT INTO Next_to_send (file_id,group_id, mes_type) VALUES (?,?,?);", inserting)
+            conn.commit()
+            if mes_type == "Photo":                                                             #если категория Картинки
+                photo = last_sent
+                sender = await bot.send_photo(admin_id, photo, send)
+            elif mes_type == "Video":                                                             #если категория Видео
+                photo = last_sent
+                sender = await bot.send_video(admin_id,photo, caption=send)
+            elif mes_type == "Animation":                                                             #если категория Видео
+                photo = last_sent
+                sender = await bot.send_animation(admin_id, photo, caption=send)
+            elif mes_type == "Document":                                                             #если категория Видео
+                photo = last_sent
+                sender = await bot.send_document(admin_id, photo, caption=send)
+    return sender 
