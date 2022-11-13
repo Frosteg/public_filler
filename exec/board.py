58d5dac405c1d0fece6942052fd38eddf4be3bc0
Frost_ark <frost.ark.90@gmail.com>
2022-11-13 13:20:56 +0300

    with time tracking

--- exec/board.py
+++ exec/board.py
@@ -1,101 +1,132 @@
-from re import A
+import re
 from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 from connector import dp, bot
-from data.config import admin_id
-from data.dict import ANSWER_START
+from data.config import admin_id, main_start1, main_end1, date_time, end
 from random import randint
-from exec import give_bot
-from exec import presender
-from exec import post_checker
-import datetime
+from exec import give_bot, presender, post_checker, deleter
+#from exec.post_checker import main_result
 import asyncio
 import time
+give = True
+take = True
 
 
-
-def time_Main1(main_start1, main_end1, date_time):
-        return main_start1 <= date_time <= main_end1
-
-
-main_start1 = datetime.time(8,00,0)
-main_end1 = datetime.time(23,59,50)
-
-date_time = datetime.datetime.now().time()
+async def time_Main1():
+    global delta_time
+    delta_time = main_start1 <= date_time <= main_end1
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     global give
     global take  
-    give = True
-    take = True
     print(take)
     print(give)
     keyboard_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard_markup.add('Запустить отправку','Остановить отправку')
-    await message.reply(ANSWER_START)
-    await bot.send_message(message.chat.id,f'Парсинг: {take}\nОтправка: {give}\nБот готов к запуску', reply_markup=keyboard_markup)
+    #await message.reply(ANSWER_START)
+    await message.reply(f"Привет Этот бот будет отправлять посты\nс {main_start1} до {main_end1}\nПринимать контент этот бот может круглосуточно.\nКак часто будут выходить посты?\nОтправь мне текст 'Время X', где X это количество минут")
 
-
-@dp.message_handler(Text(contains='Время'))
+@dp.message_handler(Text(contains='Время',ignore_case=True))
 async def time_send(message: types.Message):
-    
     global str_msg
     usermsg = message.text
+    #usermsg = usermsg.capitalize
     if usermsg == 'Время':
         return 
     str_msg = (str(usermsg))
     str_msg = str_msg.replace("Время ","")
     str_msg = int(str_msg)
+    await message.reply(f"Твои посты буду выходить раз в {str_msg} минут.\nТеперь нужно установить погрешность, чтобы твои посты не отправлялись в одно и тоже время.\nОтправь мне текст 'Задержка X', где X это количество минут.")
 
-@dp.message_handler(Text(contains='Задержка'))
+@dp.message_handler(Text(contains='Задержка',ignore_case=True))
 async def time_send(message: types.Message):
     global str_del
     userdel = message.text
+    #userdel = userdel.capitalize
     if userdel == 'Задержка':
         return 
     str_del = (str(userdel))
     str_del = str_del.replace("Задержка ","")
     str_del = int(str_del)
+    await message.reply(f"Отлично. Погрешность составляет {str_del} минут.\nТвои посты буду выходить раз в {str_msg} с погрешностью {str_del}.\nЧтобы начать работать отправь боту текст 'Запустить отправку' либо нажми кнопку на клавиатуре бота")
 
 async def delay_count():
     global delay
     global post
+    global time_local
     delay = 60 * (str_msg + randint(0, str_del))
     nowTime = time.time()
     newdelay = nowTime + delay
     time_local = time.localtime(newdelay)
     time.strftime('%H:%M:%S', time_local)
     post = time.strftime('%H:%M:%S', time_local)
-    
 
-@dp.message_handler(Text(equals='Запустить отправку'))
+async def time_send(delay):
+    global give
+    end = 1440
+    min_send = delay/60
+    nowTime = time.time()
+    time_local = time.localtime(nowTime)
+    hour = time_local[3] * 60
+    minute = time_local[4]
+    minute_total_now = hour + minute
+    difference = end - minute_total_now
+    print(difference)
+    diff = min_send > difference
+    print(diff)
+    if diff == True:
+        give = False
+       
+@dp.message_handler(Text(equals='Запустить отправку',ignore_case=True))
 async def process_parser_command(message: types.Message):
     global give
+    global posts
     give = True
     print(give)
-    await bot.send_message(message.chat.id, f'Отправка: {give}\nОтправка запущена')
+    await bot.send_message(message.chat.id, 'Запускаю отправку')
     while give == True:
-        date_time = datetime.datetime.now().time()
-        if time_Main1(main_start1, main_end1, date_time):
+        await time_Main1()
+        if delta_time:
             await delay_count()
-            await presender.presend_smthg()
-            posts = post_checker.check_post()
-            await bot.send_message(admin_id, f'Следующий пост в {post}\n{posts}')
-            await asyncio.sleep(delay) 
-            if time_Main1(main_start1, main_end1, date_time) and give == True:
-                await give_bot.send_smthg()           
+            await time_send(delay)
+            print(give)
+            if delta_time and give == True:
+                posts = post_checker.check_post()
+                main_result = post_checker.main_result
+                if main_result == 0:
+                    await bot.send_message(admin_id, f"{posts}. Отпрвлять нечего")
+                    while main_result == 0:
+                        await asyncio.sleep(5)
+                        main_result = post_checker.main_result                        
+                else:
+                    await presender.presend_smthg()
+                    await bot.send_message(admin_id, f'Следующий пост в {post}\n{posts}')
+                    await asyncio.sleep(delay) 
+                    await give_bot.send_smthg()           
         else:
             await asyncio.sleep(5)
-            #continue
     else:
-        await bot.send_message(message.chat.id, f'Отправка: {give}\nРабота приостановлена')
+        await bot.send_message(message.chat.id, f'Отправка приостановлена')
 
-@dp.message_handler(Text(equals='Остановить отправку'))
+@dp.message_handler(Text(equals='Удалить',ignore_case=True))
+async def process_del_command(message: types.Message):
+    await deleter.delete_smthg()
+    await message.reply('Неугодный пост удален')
+    posts = post_checker.check_post()
+    main_result = post_checker.main_result
+    if main_result == 0:
+        await bot.send_message(admin_id, f"{posts}. Отпрвлять нечего")
+    else:
+        await presender.presend_smthg()
+        await bot.send_message(admin_id, f'Следующий пост в {post}\n{posts}')
+    
+
+@dp.message_handler(Text(equals='Остановить отправку',ignore_case=True))
 async def process_parser_command(message: types.Message):
     global give
     give = False
     print(give)
-    await bot.send_message(message.chat.id,f'Отправка: {give}\nОстановка работы отправки')
+    await bot.send_message(message.chat.id,f'Остановка работы отправки')
     
