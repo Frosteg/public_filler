e794c778a2526d180fec656671712cd184fd2cac
Frost_ark <frost.ark.90@gmail.com>
2023-02-05 13:56:37 +0300

    Add files via upload

--- start.py
+++ start.py
@@ -1,15 +1,15 @@
 from connector import dp
 from aiogram.utils import executor
 import logging
-
-
-
 async def bot_online(_):  
     print('Я готов!')
 
-logging.basicConfig(level=logging.INFO)
-#logging.basicConfig(format=u'%(filename)s [ LINE:%(lineno)+3s ]#%(levelname)+8s [%(asctime)s]  %(message)s',
-                   # level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO, filename="data/py_log.log",filemode="a",format="%(asctime)s %(levelname)s %(message)s")
+logging.debug("A DEBUG Message")
+logging.info("An INFO")
+logging.warning("A WARNING")
+logging.error("An ERROR")
+logging.critical("A message of CRITICAL severity")
 
 import exec
 
